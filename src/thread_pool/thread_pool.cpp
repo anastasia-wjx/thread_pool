d47e511cd87965349d174823207f5210ef027bcb
w00453072 <wangjiaxin7@huawei.com>
2018-12-18 10:06:36 +0800

    添加注释

--- src/thread_pool/thread_pool.cpp
+++ src/thread_pool/thread_pool.cpp
@@ -8,26 +8,30 @@ void CTask::setData(void* data) {
     m_ptrData = data;
 }
 
+//静态成员初始化
 vector<CTask*> CThreadPool::m_vecTaskList;
 bool CThreadPool::shutdown = false;
 pthread_mutex_t CThreadPool::m_pthreadMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t CThreadPool::m_pthreadCond = PTHREAD_COND_INITIALIZER;
 
+//线程管理类构造函数
 CThreadPool::CThreadPool(int threadNum) {
     this->m_iThreadNum = threadNum;
     printf("I will create %d threads.\n", threadNum);
     Create();
 }
 
+//线程回调函数
 void* CThreadPool::ThreadFunc(void* threadData) {
     pthread_t tid = pthread_self();
     while(1)
     {
         pthread_mutex_lock(&m_pthreadMutex);
-
+        //如果队列为空，等待新任务进入任务队列
         while(m_vecTaskList.size() == 0 && !shutdown)
             pthread_cond_wait(&m_pthreadCond, &m_pthreadMutex);
 
+        //关闭线程
         if(shutdown)
         {
             pthread_mutex_unlock(&m_pthreadMutex);
@@ -37,7 +41,7 @@ void* CThreadPool::ThreadFunc(void* threadData) {
 
         printf("[tid: %lu]\trun: ", tid);
         vector<CTask*>::iterator iter = m_vecTaskList.begin();
-
+        //取出一个任务处理
         CTask* task = *iter;
         if(iter != m_vecTaskList.end())
         {
@@ -47,12 +51,13 @@ void* CThreadPool::ThreadFunc(void* threadData) {
 
         pthread_mutex_unlock(&m_pthreadMutex);
 
-        task->Run();
+        task->Run();  //执行任务
         printf("[tid: %lu]\tidle\n", tid);
 
     }
 }
 
+//给任务队列里添加任务并发出线程同步信号
 int CThreadPool::AddTask(CTask *task) {
     pthread_mutex_lock(&m_pthreadMutex);
     m_vecTaskList.push_back(task);
@@ -62,6 +67,7 @@ int CThreadPool::AddTask(CTask *task) {
     return 0;
 }
 
+//创建线程
 int CThreadPool::Create() {
     pthread_id = new pthread_t[m_iThreadNum];
     for(int i = 0; i < m_iThreadNum; i++)
@@ -70,26 +76,32 @@ int CThreadPool::Create() {
     return 0;
 }
 
+//停止所有线程
 int CThreadPool::StopAll() {
+    //避免重复调用
     if(shutdown)
         return -1;
     printf("Now I will end all threads!\n\n");
 
+    //唤醒所有等待线程，线程池也要销毁了
     shutdown = true;
     pthread_cond_broadcast(&m_pthreadCond);
 
+    //清除僵尸
     for(int i = 0; i < m_iThreadNum; i++)
         pthread_join(pthread_id[i], NULL);
 
     delete[] pthread_id;
     pthread_id = NULL;
 
+    //销毁互斥量和条件变量
     pthread_mutex_destroy(&m_pthreadMutex);
     pthread_cond_destroy(&m_pthreadCond);
 
     return 0;
 }
 
+//获取当前队列中的任务数
 int CThreadPool::getTaskSize() {
     return m_vecTaskList.size();
 }
